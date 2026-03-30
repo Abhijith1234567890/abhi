5b87b93ff58b61f42c12d148ad4851dddc69bbef
abhijith <abhijithpp2001@gmail.com>
2026-03-30 12:07:17 +0530

    Ready to deploy

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const app = require("./app");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 async function startServer() {
